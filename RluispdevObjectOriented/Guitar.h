a7e5fdf82d57671fad87256bb0a233a06c5f82c5
rluispdev <rluisp@icloud.com>
2024-07-16 09:05:21 -0300

    Herdar de uma classe.

---
+++ RluispdevObjectOriented/Guitar.h
@@ -0,0 +1,16 @@
+//
+//  Guitar.h
+//  RluispdevObjectOriented
+//
+//  Created by Rafael Gonzaga on 15/07/24.
+//
+
+#import "MusicalInstrument.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Guitar : MusicalInstrument
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RluispdevObjectOriented/Guitar.m
@@ -0,0 +1,12 @@
+//
+//  Guitar.m
+//  RluispdevObjectOriented
+//
+//  Created by Rafael Gonzaga on 15/07/24.
+//
+
+#import "Guitar.h"
+
+@implementation Guitar
+
+@end
